945eed9df29d330607d51d0962bbc6a57f537f78
Matthew Suderman <matthewsuderman@hotmail.com>
2025-07-28 11:09:52 +0100

    prepare complicated

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -14,9 +14,18 @@ SCRIPT=out/run-emacs.sh
 cat <<EOF > $SCRIPT
 #!/bin/bash
 
-EXT_BASE=\$(dirname \$0)
-EXT_BASE=\$(realpath \$EXT_BASE)
+EXT_BASE=\$(dirname \$(realpath \$0))
 
-apptainer run -B \$EXT_BASE:$BASE -B \$(pwd) \$EXT_BASE/$NAME.sif "\$@"
+## filename arguments can be complicated for containers
+## when they need to bindings
+# FILES=""
+# BINDS=""
+# if [ "$#" -gt 0 ]; then
+#     FILES=\$(printf -- '%s ' \$(realpath "\$@"))
+#     BINDS=\$(printf -- '-B %s ' \$(dirname \$(realpath "\$@")))
+# fi
+# apptainer run -B \$EXT_BASE:$BASE -B \$(pwd) \$BINDS \$EXT_BASE/$NAME.sif \$FILES
+
+apptainer run -B \$EXT_BASE:$BASE -B \$(pwd) \$EXT_BASE/$NAME.sif 
 
 EOF
